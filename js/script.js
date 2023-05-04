e1a64cedf052d5438338f2ef53001f42f03e09ea
Olga <olga.emel@gmail.com>
2023-05-04 17:01:57 +0300

    final only desctop

---
+++ js/script.js
@@ -0,0 +1,28 @@
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
+
+ScrollReveal().reveal('.main-title, .section-title', {
+  delay: 500,
+  origin: 'left',
+})
+ScrollReveal().reveal('.sec-01 .image, .info', { delay: 600, origin: 'bottom' })
+
+ScrollReveal().reveal('.text-box', { delay: 700, origin: 'right' })
+ScrollReveal().reveal('.media-icons i', {
+  delay: 500,
+  origin: 'bottom',
+  interval: 200,
+})
+ScrollReveal().reveal('.sec-02 .image, .sec-03 .image', {
+  delay: 500,
+  origin: 'top',
+})
+ScrollReveal().reveal('.media-info li', {
+  delay: 500,
+  origin: 'left',
+  interval: 200,
+})
